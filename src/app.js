3e9796865dcaae424d1a0941a8d6db37ffb87cf1
Dawid Ząbek <dawidzabek3@wp.pl>
2026-05-10 23:46:11 +0200

    Basic view of blog

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import './models/user.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 import postsRouter from './routes/posts.js';
 import tagsRouter from './routes/tags.js';
 import notificationsRouter from './routes/notifications.js';
@@ -9,9 +13,10 @@ import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors());
 app.use(express.json());
+app.use(express.static(join(__dirname, '..')));
 
 app.use('/api/posts', postsRouter);
 app.use('/api/tags', tagsRouter);
